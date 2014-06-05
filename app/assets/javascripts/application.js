30bc99c8efc53ba63f066c0e82f32a2e19dad9b9
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-06-05 03:52:57 -0700

    added viewport meta tag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
-//= require masonry.pkgd.js
 //= imagesloaded.pkgd.js
+//= require masonry.pkgd.js
 //= require turbolinks
 //= require_tree .
